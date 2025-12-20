97b9634ed75905942fd413ead8315ac6f7d55be6
dhrupadraj <dhraupad@gmail.com>
2025-12-20 18:04:51 +0530

    changes made

--- ui.py
+++ ui.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from utils import is_valid_url
+
 
 # Set up sidebar
 with st.sidebar:
@@ -6,6 +8,12 @@ with st.sidebar:
     product_url = st.text_input("Product URL")
     add_button = st.button("Add Product")
 
+    if add_button:
+        if not product_url:
+            st.error("Please enter a product URL")
+        elif not is_valid_url(product_url):
+            st.error("Please enter a valid URL")
+        else:
+            st.success("Product is now being tracked!")
+
 # Main content
-st.title("Price Tracker Dashboard")
-st.markdown("## Tracked Products")
